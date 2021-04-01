b79710882683dab3e0cced50e5352ea6e5a15e4b
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-04-01 18:22:55 -0400

    through exercise5

---
+++ answers/exercise3.sql
@@ -0,0 +1,6 @@
+create table myNewDB.Users
+    -> (id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        -> LastName VARCHAR(255) NOT NULL,
+    -> FirstName VARCHAR(255) NOT NULL,
+    -> Address VARCHAR(255) NOT NULL,
+    -> CITY VARCHAR(255) NOT NULL);
